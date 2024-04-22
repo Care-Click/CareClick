d2ce6a8a2f7768f2db7e38f9a523853160a5074e
Omarboubaker00 <omar.boubakker00@gmail.com>
2024-04-22 14:48:37 +0100

    worked sign in

--- back-end/controllers/doctors.js
+++ back-end/controllers/doctors.js
@@ -1,4 +1,3 @@
-const prisma = require("../db/prisma");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -7,14 +6,26 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const signup = async (req, res) => {
+  let { password, date_of_birth, FullName, email, phone_number, gender, location } = req.body;
+  date_of_birth = new Date(date_of_birth);
+  date_of_birth = date_of_birth.toISOString();
   try {
-    let { password, date_of_birth, FullName, email, phone_number, gender, location } = req.body;
-    date_of_birth = new Date(date_of_birth);
-    date_of_birth = date_of_birth.toISOString();
-
+    //checking if the email is already in use
+    const checkemail = await prisma.doctor.findUnique({ where: { email: email } });
+    if (checkemail) {
+      return res.status(400).json({ error: "existing account  " });
+    }
     // Hashing the password
     const hashedPassword = bcrypt.hashSync(password, 8);
-    const imageBuffer = req.files;
+    // Check if req.files is defined and contains the file buffer
+    if (!req.files || !req.files[0].buffer) {
+      return res.status(400).json({ error: "Image buffer is missing from request" });
+    }
+
+    // Assuming req.files contains the image buffer, adjust this accordingly based on your setup
+    const imageBuffer = req.files[0].buffer;
+
+    // Uploading image to Cloudinary
     const imageUrl = await upload(imageBuffer);
 
     const newDoctor = {
@@ -24,11 +35,11 @@ const signup = async (req, res) => {
       date_of_birth,
       phone_number,
       gender,
-      imageUrl,
       location,
       role: 'doctor', // Assuming doctor signup
-      verified: true,
-      status: true
+      verified: true, // Verifie from the admin
+      status: true,
+      profile_picture: imageUrl
     };
 
     // Save doctor data to the database
@@ -41,72 +52,72 @@ const signup = async (req, res) => {
   }
 };
 
-const signin = async (req, res) => { 
+const signin = async (req, res) => {
   const { email, password } = req.body;
 
-if (!email || !password) {
-  return res.status(404).json({ error: "Email or Password not found." });
-}
-
-try {
-  // Retrieve doctor from the database by email
-  const doctor = await prisma.doctor.findUnique({
-    where: {
-      email: email
-    }
-  });
-
-  // Check if doctor exists
-  if (!doctor) {
-    return res.status(404).json({ error: "User not found." });
+  if (!email || !password) {
+    return res.status(404).json({ error: "Email or Password not found." });
   }
 
-  // Checking if the password is valid
-  const passwordMatch = await bcrypt.compare(password, doctor.password);
+  try {
+    // Retrieve doctor from the database by email
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        email: email
+      }
+    });
 
-  if (!passwordMatch) {
-    return res.status(401).json({ error: "Password is incorrect." });
-  }
-
-  // Generate a JSON Web Token (JWT) for authentication
-  const token = jwt.sign(
-    {
-      userId: doctor.id,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "1d",
+    // Check if doctor exists
+    if (!doctor) {
+      return res.status(404).json({ error: "User not found." });
     }
-  );
 
-  let loggedUser = {
-    id: doctor.id,
-    FullName: doctor.FullName
-  };
+    // Checking if the password is valid
+    const passwordMatch = await bcrypt.compare(password, doctor.password);
 
-  res.status(200).json({ loggedUser, token, message: "Login succeeded" });
-} catch (error) {
-  console.error("Sign in error:", error);
-  res.status(500).send("Internal server error");
-}
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Password is incorrect." });
+    }
+
+    // Generate a JSON Web Token (JWT) for authentication
+    const token = jwt.sign(
+      {
+        userId: doctor.id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    let loggedUser = {
+      id: doctor.id,
+      FullName: doctor.FullName
+    };
+
+    res.status(200).json({ loggedUser, token, message: "Login succeeded" });
+  } catch (error) {
+    console.error("Sign in error:", error);
+    res.status(500).send("Internal server error");
+  }
 };
 
-const getOne = async (req, res) => {};
+const getOne = async (req, res) => { };
 
-const sendReq = async (req, res) => {};
+const sendReq = async (req, res) => { };
 
-const search = async (req, res) => {};
+const search = async (req, res) => { };
 
-const updatePatientMed = async (req, res) => {};
+const updatePatientMed = async (req, res) => { };
 
 
 const getAllPatient = async (req, res) => {
   console.log("😎");
-try {
-  let result= await prisma.patient.findMany()
-  console.log(result); 
-  res.status(200).json(result)
-} catch (err) {
+  try {
+    let result = await prisma.patient.findMany()
+    console.log(result);
+    res.status(200).json(result)
+  } catch (err) {
     console.log(err);
     res.status(404).json({ error: " not found." })
   }
