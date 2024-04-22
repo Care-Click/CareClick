3c605544936c5cb5891746aea77a74f4dcca40a1
Omarboubaker00 <omar.boubakker00@gmail.com>
2024-04-22 23:27:49 +0100

    add the route of medical experience

--- ClientWeb/src/App.tsx
+++ ClientWeb/src/App.tsx
@@ -4,19 +4,22 @@ import Navbar from "./components/auth/Navbar.tsx";
 import Login from "./components/auth/Login.tsx";
 import SignUp from "./components/auth/SignUp.tsx";
 import Allpatients from "./components/doctor/Allpatients.tsx";
+import MedicalExp from "./components/auth/MedicalExp.tsx";
 
 
 const App = () => {
   return (
     <div className="App" >
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/login" element={<Login  />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/contact" element={<Contact />} /> */}
         <Route path="/join-us" element={<SignUp />} />
 
         <Route path="/allpatient" element={<Allpatients />} />
-      
+        <Route path="/medicalExp" element={<MedicalExp />} />
+
+
       </Routes>
     </div>
   )
