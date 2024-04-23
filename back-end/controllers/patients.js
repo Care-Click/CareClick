7fea283dfcb3029a055a32bbf7105d097d877f6a
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-23 10:14:30 +0100

    patient sign up

--- back-end/controllers/patients.js
+++ back-end/controllers/patients.js
@@ -1,17 +1,23 @@
 const prisma = require("../db/prisma");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const randomNumber=require('../utils/random')
 const signup = async (req, res) => {
   try {
     let { password, date_of_birth } = req.body;
     date_of_birth = new Date(date_of_birth);
     date_of_birth = date_of_birth.toISOString();
     const newPatient = { ...req.body, date_of_birth };
-
+    if(!req.files)
+    if (newPatient.Gender==="female") {
+      newPatient.profile_picture="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ-s3t28RdEh3CJTtyc5ixDrT1oRDoHGPENQQrhnz1kev-OO4tq"
+    } else {
+      newPatient.profile_picture="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYEIBbdsxOb3ohy2QYPCKVCKsvnYyb4WwiVFiWf9DdhOEaa7I"
+    }
     newPatient.password = bcrypt.hashSync(password, 8);
-
+    newPatient.verification_code = randomNumber();
+    newPatient.role = "patient";
+    newPatient.location = JSON.stringify(newPatient.location);
     let result = await prisma.patient.create({ data: newPatient });
 
     res.status(201).send("Patient  Registred ");
